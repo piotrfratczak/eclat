ec44acf3a4298f457c4fd1f51e94c2d1fd555952
Piotr Frątczak <piotrfratczak99@gmail.com>
2022-06-07 17:21:49 +0200

    Add execution arguments ECLAT execution parametrizaton

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--dataset', type=int, default='0')
     parser.add_argument('--data', type=str)
     parser.add_argument('--taxonomy', type=str)
+    parser.add_argument('--min_sup', type=int, default=1)
+    parser.add_argument('--min_conf', type=float, default=0.5)
+    parser.add_argument('--min_len', type=int, default=1)
+    parser.add_argument('--max_len', type=int)
     args = parser.parse_args()
     print(args)
     return args
@@ -21,7 +25,12 @@ def main() -> None:
         transactions, taxonomy = load_dataset(args.data, args.taxonomy)
     else:
         transactions, taxonomy = load_predefined(Dataset(args.dataset))
-    rules = eclat(transactions, taxonomy=taxonomy)
+    rules = eclat(transactions,
+                  taxonomy=taxonomy,
+                  min_sup=args.min_sup,
+                  min_conf=args.min_conf,
+                  min_len=args.min_len,
+                  max_len=args.max_len)
     save_rules(rules)
 
 
